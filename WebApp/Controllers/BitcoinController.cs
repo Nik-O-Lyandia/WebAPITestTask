d7b6c2f57bbe79d451b4e23e34fb00117f832a2f
Mykola Kovalenko <kovnikur@gmail.com>
2022-07-25 22:00:08 +0300

    Added project files

---
+++ WebApp/Controllers/BitcoinController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Services;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BitcoinController : Controller
+    {
+        BitcoinService _bitcoinSrvice;
+
+        public BitcoinController(IHttpClientFactory clientFactory)
+        {
+            _bitcoinSrvice = new BitcoinService(clientFactory);
+        }
+
+        // GET: BitcoinController
+        [HttpGet("rate")]
+        public ActionResult Rate()
+        {
+            try
+            {
+                return Ok(_bitcoinSrvice.Rate());
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost("subscribe")]
+        public ActionResult Subscribe(string email)
+        {
+            try
+            {
+                _bitcoinSrvice.Subscribe(email);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return Conflict(ex.Message);
+            }
+            catch
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        [HttpPost("sendEmails")]
+        public ActionResult SendEmails()
+        {
+            try
+            {
+                _bitcoinSrvice.SendEmails();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ WebApp/Services/BitcoinService.cs
@@ -0,0 +1,103 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using WebApp.Models;
+
+namespace WebApp.Services
+{
+    public class BitcoinService
+    {
+        private const string _emailsFilePath = "Src/emails.txt";
+        IHttpClientFactory _clientFactory;
+
+        public BitcoinService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+        public decimal Rate()
+        {
+            var client = _clientFactory.CreateClient();
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                "https://bitpay.com/api/rates");
+            var response = client.Send(request);
+
+            var contentStream = response.Content.ReadAsStream();
+            var currencies = JsonSerializer.Deserialize<List<Currency>>(contentStream);
+
+            Currency finalCurrency = new Currency();
+
+            if (currencies != null)
+            {
+                foreach (Currency cur in currencies)
+                {
+                    if (cur.Code == "UAH")
+                    {
+                        finalCurrency = cur;
+                    }
+                }
+            }
+
+            return finalCurrency.Rate;
+        }
+
+        public void Subscribe(string email)
+        {
+            Regex rgx = new Regex("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~]+[@][a-z]+[.][a-z]+");
+
+            if (rgx.IsMatch(email))
+            {
+                var allEmails = File.ReadAllLines(_emailsFilePath);
+
+                if (!allEmails.Contains(email))
+                {
+                    File.AppendAllText(_emailsFilePath, email + "\n");
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Address '{email}' is already subscribed");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Wrong email address");
+            }
+        }
+
+        public void SendEmails()
+        {
+            var allEmails = File.ReadAllLines(_emailsFilePath);
+
+            if (allEmails != null)
+            {
+                var rate = Rate();
+                //string to = "toaddress@gmail.com"; //To address    
+                //string from = "fromaddress@gmail.com"; //From address    
+                //MailMessage message = new MailMessage(from, to);
+
+                //string mailbody = "In this article you will learn how to send a email using Asp.Net & C#";
+                //message.Subject = "Sending Email Using Asp.Net & C#";
+                //message.Body = mailbody;
+                //message.BodyEncoding = Encoding.UTF8;
+                //message.IsBodyHtml = true;
+                //SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //Gmail smtp    
+                //System.Net.NetworkCredential basicCredential1 = new
+                //System.Net.NetworkCredential("yourmail id", "Password");
+                //client.EnableSsl = true;
+                //client.UseDefaultCredentials = false;
+                //client.Credentials = basicCredential1;
+                //try
+                //{
+                //    client.Send(message);
+                //}
+
+                //catch (Exception ex)
+                //{
+                //    throw ex;
+                //}
+            }
+        }
+    }
+}
